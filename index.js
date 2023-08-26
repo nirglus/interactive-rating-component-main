a82f30ef581eaed61f991c4736035ed6b298bc8d
nirglus <nirgluskin@gmail.com>
2023-08-26 18:01:52 +0300

    add loop for reuslt display

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const mainCard = document.querySelector(".card");
 const thanksCard = document.querySelector(".thanksCard");
-const dig = document.getElementsByClassName("dig");
+const results = document.getElementsByClassName("dig");
 const submitBtn = document.querySelector(".btn");
-const userResult = document.getElementsByClassName("user-result");
+const userResult = document.querySelector(".user-result");
+
 submitBtn.addEventListener("click", function(){
     thanksCard.classList.remove("hidden");
     mainCard.classList.add("hidden");
 });
+
+for(let i = 0; i < results.length; i++){
+    results[i].addEventListener("click", function(){
+        userResult.innerHTML = results[i].innerHTML;
+    });
+}
