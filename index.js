1adc30f8a6ec857593542f9b935d0eaa59d7160f
nirglus <nirgluskin@gmail.com>
2023-08-26 17:40:40 +0300

    add event listener for btn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const mainCard = document.getElementsByClassName("card");
-const thanksCard = document.getElementsByClassName("thanCard");
+const mainCard = document.querySelector(".card");
+const thanksCard = document.querySelector(".thanksCard");
 const dig = document.getElementsByClassName("dig");
+const submitBtn = document.querySelector(".btn");
+const userResult = document.getElementsByClassName("user-result");
+submitBtn.addEventListener("click", function(){
+    thanksCard.classList.remove("hidden");
+    mainCard.classList.add("hidden");
+});
